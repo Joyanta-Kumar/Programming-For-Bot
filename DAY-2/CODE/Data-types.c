2ef8116fdc5e7aa64a2f9444d0be1dae6542f70a
Joyanta Kumar <joyroy122004@gmail.com>
2025-01-03 01:23:05 +0600

    Text styling

--- DAY-2/CODE/Data-types.c
+++ DAY-2/CODE/Data-types.c
@@ -5,10 +5,10 @@ int main()
     int roll_number = 54;
     float attendance = 0.73;    // 73% = 0.73
     double scholarship = 0.0000021;
-    char group = 'M';
+    char group = 'M';\
 
 
-    printf(" \e[4mVariable      Value            Data type       Size\n\e[0m");   // "\n" for new line.
+    printf("\n \e[4mVariable      Value            Data type       Size\n\e[0m");   // "\n" for new line.
     printf("\e[32m Roll number\e[37m:\e[33m\t%d    \t\t\e[34mint   \t\t\e[37m%d B\n", roll_number, sizeof(int));     // "\t" for tab
     printf("\e[32m Attendance \e[37m:\e[33m\t%g %%   \t\e[34mfloat \t\t\e[37m%d B\n", attendance, sizeof(float));      // "%%" will show up once
     printf("\e[32m Scholarship\e[37m:\e[33m\t%.8lf $ \t\e[34mdouble\t\t\e[37m%d B\n", scholarship, sizeof(double));
