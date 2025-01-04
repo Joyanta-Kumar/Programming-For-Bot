838843ea322b4081e33f91dcf7b55a2d31d4ebbf
Joyanta Kumar <joyroy122004@gmail.com>
2025-01-04 15:21:02 +0600

    Fixing mistakes of previous day (DAY-3)

---
+++ DAY-4/Mistakes-Of-Day-3/Char-To-Int.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+// Store Char -> Int is possible. [x] I have 4B, I store 1B.
+
+int main() {
+    int a;
+    printf("Input: ");
+    scanf("%c", &a);
+    printf("%d", a);
+    return 0;
+}
