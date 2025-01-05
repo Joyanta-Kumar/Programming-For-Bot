e5ff1a795db1cad00a032de82173fe8530ff038e
Joyanta Kumar <joyroy122004@gmail.com>
2025-01-05 23:23:29 +0600

    h

--- DAY-4/CODE/Count-Even.c
+++ DAY-4/CODE/Count-Even.c
@@ -3,7 +3,15 @@
 int main()
 {
     int count = 0;
-    for (int i = 0; i <= 20; i++, count += (i % 2 == 0) ? 1 : 0) {}
+
+    for (int i = 0; i <= 20; i++)
+    {
+        if (i % 2 == 1)
+        {
+            count++;
+        }
+    }
+
     printf("%d\n", count);
-    return 0;
+    return 0; 
 }
